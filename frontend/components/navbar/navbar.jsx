cf842530cc0f75c5cc0d9b8f8a702fba1ea7fe85
Nandhu <knandhu.27@gmail.com>
2020-03-08 20:46:15 -0700

    Footer update

--- frontend/components/navbar/navbar.jsx
+++ frontend/components/navbar/navbar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import { Route, Redirect, Switch, Link, HashRouter, withRouter } from "react-router-dom";
 import QuestionsContainer from "./../questions/questions_container";
 import { searchQuestions } from './../../util/search_questions_api_util';
-import SearchBarContainer from './../questions/search_bar_container';
+// import SearchBarContainer from './../questions/search_bar_container';
 // import SearchPage from "../questions/search_page";
 class NavBar extends React.Component {
   constructor(props) {
