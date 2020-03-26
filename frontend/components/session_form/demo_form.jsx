dcc3c194761a539b6d9c9822522c074382ed34cc
Nandhu <knandhu.27@gmail.com>
2020-03-25 23:08:34 -0700

    Demo login

--- frontend/components/session_form/demo_form.jsx
+++ frontend/components/session_form/demo_form.jsx
@@ -2,65 +2,108 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 class DemoForm extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            email: "email@email.com",
-            password: "password"
-        };
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
-    update(field) {
-        return (e => (
-            this.setState({ [field]: e.target.value })
-        ));
-    }
-    handleSubmit(e) {
-        e.preventDefault();
-        const user = Object.assign({}, this.state);
-        this.props.processForm(user).then(()=>this.props.history.push('/questions'));
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: ""
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  update(field) {
+    return e => this.setState({ [field]: e.target.value });
+  }
+  handleSubmit(e) {
+    e.preventDefault();
+    // const user = Object.assign({}, this.state);
+    const user = {
+      email: "email@email.com",
+      password: "password"
+    };
+    this.demo(user);
+    setTimeout(() => {
+      this.props
+        .processForm(user)
+        .then(() => this.props.history.push("/questions"));
+    }, 2000);
+  }
 
-    render() {
-        return (
-          <div className="demo-form">
-            <form className="demo-box" onSubmit={this.handleSubmit}>
-              <div className="demo-fields">
-                <label htmlFor="">
-                  Email
-                  <br />
-                  <input
-                    type="text"
-                    className="demo-input"
-                    value={this.state.email}
-                    onChange={this.update("email")}
-                  />
-                </label>
-                <br />
-                <label htmlFor="">
-                  Password
-                  <br />
-                  <input
-                    type="password"
-                    className="demo-input"
-                    value={this.state.password}
-                    autoComplete="current-password"
-                    onChange={this.update("password")}
-                  />
-                </label>
-                <br />
+  demo(user) {
+    const intervalSpeed = 75;
+    const { email, password } = user;
+    const demoEmailTime = email.length * intervalSpeed;
+    const demoPasswordTime = password.length * intervalSpeed;
+    const buffer = intervalSpeed * 2;
+    const totalDemoTime = demoEmailTime + demoPasswordTime + buffer;
+    this.demoEmail(email, intervalSpeed);
+    setTimeout(() => this.demoPassword(password, intervalSpeed), demoEmailTime);
+    setTimeout(() => this.props.login(user), totalDemoTime + 100);
+  }
+  demoEmail(email, intervalSpeed) {
+    let i = 0;
+    setInterval(() => {
+      if (i <= email.length) {
+        this.setState({ email: email.slice(0, i) });
+        i++;
+      } else {
+        clearInterval();
+      }
+    }, intervalSpeed);
+  }
+  demoPassword(password, intervalSpeed) {
+    let j = 0;
+    setInterval(() => {
+      if (j <= password.length) {
+        this.setState({ password: password.slice(0, j) });
+        j++;
+      } else {
+        clearInterval();
+      }
+    }, intervalSpeed);
+  }
 
-                <input
-                  className="demo-submit"
-                  type="submit"
-                  value={this.props.formType}
-                />
-                <br />
-              </div>
-            </form>
+  render() {
+    return (
+      <div className="demo-form">
+        <form className="demo-box" onSubmit={this.handleSubmit}>
+          <div className="demo-fields">
+            <label htmlFor="">
+              Email
+              <br />
+              <input
+                type="text"
+                className="demo-input"
+                placeholder="Email"
+                value={this.state.email}
+                onChange={this.update("email")}
+              />
+            </label>
+            <br />
+            <label htmlFor="">
+              Password
+              <br />
+              <input
+                type="password"
+                placeholder="Password"
+                className="demo-input"
+                value={this.state.password}
+                autoComplete="current-password"
+                onChange={this.update("password")}
+              />
+            </label>
+            <br />
+
+            <input
+              className="demo-submit"
+              type="submit"
+              value={this.props.formType}
+            />
+            <br />
           </div>
-        );
-    }
+        </form>
+      </div>
+    );
+  }
 }
 
 export default DemoForm;
