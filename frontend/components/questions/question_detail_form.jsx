475b0dd17c127d3c3d982bddddddf6034a699edf
Nandhu <knandhu.27@gmail.com>
2020-03-28 13:57:27 -0700

    cursor change

--- frontend/components/questions/question_detail_form.jsx
+++ frontend/components/questions/question_detail_form.jsx
@@ -7,7 +7,7 @@ import Footer from './../home/footer';
 import AnswerForm from './../answers/answer_form';
 import AnswerContainer from '../answers/answer_container';
 import CreateAnswerFormContainer from './../answers/create_answer_form_container';
-import { createHistory } from 'history';
+// import { createHistory } from 'history';
 import Answer from './../answers/answer';
 import ReactQuill from "react-quill";
 
