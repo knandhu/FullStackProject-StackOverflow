ae34eda3eafa4e6cafbbde31b02dd2c4028eb07c
Nandeeswari Kuppusamy <knandhu.27@gmail.com>
2020-06-19 12:49:50 -0700

    update tag actions

--- frontend/actions/tag_actions.js
+++ frontend/actions/tag_actions.js
@@ -1,6 +1,6 @@
 import { fetchTags } from "./../util/tag_api_util";
 export const RECEIVE_TAGS = "RECEIVE_TAGS";
-// export const CLEAR_SEARCH = "CLEAR_SEARCH";
+
 
 const recieveTags = tags => ({
   type: RECEIVE_TAGS,
