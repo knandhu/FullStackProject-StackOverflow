f2706f23f3e8938fca4fd1ca5e49f898aa5a1720
Nandeeswari Kuppusamy <knandhu.27@gmail.com>
2020-08-15 00:32:00 -0700

    Update tag.rb

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -6,7 +6,7 @@
 #  name       :string           not null
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
-#
+
 
 class Tag < ApplicationRecord
     validates :name, presence:true
