9c2e69cda3fd27f1061e56b110f3690d8aa0aa5a
Nandeeswari Kuppusamy <knandhu.27@gmail.com>
2020-09-21 22:13:32 -0700

    Update store.js

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -2,7 +2,7 @@ import { createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
 import thunk from "redux-thunk";
 import rootReducer from "../reducers/root_reducer";
-
+//
 const configureStore = (preloadedState = {}) => {
   let middleware = [thunk];
   if (process.env.NODE_ENV !== "production") {
