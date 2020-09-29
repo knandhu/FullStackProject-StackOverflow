9f7eb11de880504c6c3948bb372360233a6e739b
Nandeeswari Kuppusamy <knandhu.27@gmail.com>
2020-09-29 14:24:38 -0700

    Update tags_container.js

--- frontend/components/tags/tags_container.js
+++ frontend/components/tags/tags_container.js
@@ -1,7 +1,7 @@
 import { connect } from 'react-redux';
 import { receiveTags } from './../../actions/tag_actions';
 import TagsIndex from './tags_index';
-
+//
 const mapStateToProps = state => {
     
     return {
